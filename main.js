cb57d3f881e5de34657d668586c17bd54f6f072c
ArtemKiP <der.pap@mail.ru>
2023-03-15 20:29:05 +0600

    dom-js

---
+++ main.js
@@ -0,0 +1,42 @@
+// let id = document.querySelector('#title');
+//
+//
+// console.log(id);
+
+
+// let text = document.querySelectorAll('.text');
+//
+//
+// console.log(Array.from(text));
+
+
+// let li = document.querySelectorAll('li.list-item');
+//
+// console.log(Array.from(li));
+
+
+// let li = document.querySelectorAll('li');
+//
+// Array.from(li).forEach((item,idx) => {
+// item.textContent = `${item.textContent} апчихба ${idx + 1}`
+// });
+
+// let li = document.querySelectorAll('li');
+//
+// Array.from(li).forEach((item,idx) => {
+//     if (idx % 2 !== 0){
+//         item.style.color = 'red'
+//     }else {
+//         item.style.color = 'gold'
+//     }
+// });
+
+// let inf = document.querySelectorAll('.info');
+//
+// Array.from(inf).forEach((item,idx)=>{
+//     if (idx % 2 !== 0){
+//         item.style.color = 'red'
+//     }else {
+//         item.style.color = 'green'
+//     }
+// });
